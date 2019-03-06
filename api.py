7a198294f75b5d5df61dcb513741ab4b8e15eec6
minyeong, Yang <ymy802@kaist.ac.kr>
2019-03-06 17:54:30 +0900

    Fixed aiohttp

--- api.py
+++ api.py
@@ -1,22 +1,33 @@
 import asyncio
 import aiohttp
+from urllib import parse
 
 import utils
 from params import *
 
+
+def encode(base, url):
+    parsed = parse.urlparse(url)
+    parsed = parse.parse_qs(parsed.query)
+    encoded = "{}?{}".format(base, parse.urlencode(parsed, doseq=True))
+    return encoded
 
 class RaiderIO:
     BASE = "https://raider.io/api/v1/"
 
     @classmethod
     async def get_character_info(cls, region, realm, name):
-        url = cls.BASE + "characters/profile" \
-              + "?region={}".format(region) \
-              + "&realm={}".format(REALMS[realm]) \
-              + "&name={}".format(name) \
-              + "&fields=mythic_plus_scores,mythic_plus_weekly_highest_level_runs"
-        response = await aiohttp.request("GET", url)
-        return response
+        query = "?region={}".format(region) \
+                + "&realm={}".format(REALMS[realm]) \
+                + "&name={}".format(name) \
+                + "&fields=mythic_plus_scores,mythic_plus_weekly_highest_level_runs"
+        url = encode(cls.BASE + "characters/profile", query)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    return await response.json()
+                else:
+                    return None
 
 
 class Blizzard:
